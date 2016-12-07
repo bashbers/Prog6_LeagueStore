0b2e3ee29508d5380c97d1dbc6af9cabe065be34
Stino <Linksonder@gmail.com>
2016-12-07 09:30:59 +0100

    initial commit

---
+++ LeagueStore/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LeagueStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult News()
+        {
+            ViewBag.Titel = "SUPER MEGA KORTING";
+            ViewBag.Bericht = "Alles nu 80% korting! Opruim weken bij de League store. ";
+            ViewBag.DatumVan = DateTime.Now.ToShortDateString();
+            ViewBag.DatumTot = DateTime.Now.AddDays(7).ToShortDateString();
+
+            return View();
+        }
+    }
+}
