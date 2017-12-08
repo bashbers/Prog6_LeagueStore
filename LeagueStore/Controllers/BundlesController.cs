822641c5b42546ecdd9424f4f6c783a435d23f4d
Brian <bashbers@avans.nl>
2017-12-08 13:43:38 +0100

    Save

---
+++ LeagueStore/Controllers/BundlesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using LeagueStore.Models;
+
+namespace LeagueStore.Controllers
+{
+    public class BundlesController : Controller
+    {
+        private MyContext db = new MyContext();
+
+        // GET: Bundles
+        public ActionResult Index()
+        {
+            return View(db.Bundles.ToList());
+        }
+
+        // GET: Bundles/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Bundle bundle = db.Bundles.Find(id);
+            if (bundle == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bundle);
+        }
+
+        // GET: Bundles/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Bundles/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Description,RiotPoints,BannerUrl")] Bundle bundle)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Bundles.Add(bundle);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(bundle);
+        }
+
+        // GET: Bundles/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Bundle bundle = db.Bundles.Find(id);
+            if (bundle == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bundle);
+        }
+
+        // POST: Bundles/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Description,RiotPoints,BannerUrl")] Bundle bundle)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(bundle).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(bundle);
+        }
+
+        // GET: Bundles/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Bundle bundle = db.Bundles.Find(id);
+            if (bundle == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bundle);
+        }
+
+        // POST: Bundles/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Bundle bundle = db.Bundles.Find(id);
+            db.Bundles.Remove(bundle);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- LeagueStore/Models/Bundle.cs
+++ LeagueStore/Models/Bundle.cs
@@ -8,11 +8,6 @@ namespace LeagueStore.Models
 {
     public class Bundle
     {
-        public Bundle()
-        {
-            this.Products = new List<Product>();
-        }
-
         [Key]
         public int Id { get; set; }
 
@@ -25,5 +20,10 @@ namespace LeagueStore.Models
         public String BannerUrl { get; set; }
 
         public ICollection<Product> Products { get; set; }
+
+        public Bundle()
+        {
+            this.Products = new List<Product>();
+        }
     }
 }
